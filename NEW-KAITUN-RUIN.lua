236cf84eb7447869540d6680ef1c54019b49af71
xiaoxinyou27 <activednow1@gmail.com>
2026-03-01 01:48:26 +0800

    NEW-KAITUN-RUIN.lua

--- NEW-KAITUN-RUIN.lua
+++ NEW-KAITUN-RUIN.lua
@@ -1,5 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
-
 _G.FishItConfig = _G.FishItConfig or {
     ["Fishing"] = {
         ["Auto Perfect"] = false,
