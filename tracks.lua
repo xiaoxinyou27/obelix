432e80f1cd518996af01e33c836c56e9bed9228c
xiaoxinyou27 <activednow1@gmail.com>
2026-03-09 03:00:15 +0800

    Create tracks.lua

---
+++ tracks.lua
@@ -0,0 +1,3 @@
+_G.FLEET_ENDPOINT = "https://dashboard.amer.web.id/report/"
+_G.FLEET_TOKEN    = "ALEKT3Fv8QzN1XyR6k2D0pL5aC7H9jU4bWtMs"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/au290/roblog/refs/heads/main/trackstat.lua"))()
